e70e37920e99f23c8de016ef87065418c9344e6f
Hoàng Quang Huy <huyhoangyii@gmail.com>
2026-09-03 16:40:54 +0700

    thay đổi button xóa ở tìm kiếm

--- src/components/search/SearchPreview.tsx
+++ src/components/search/SearchPreview.tsx
@@ -11,9 +11,10 @@ export default function SearchPreview({search,onClose}:{search:string,onClose:()
         <div className="relative">
             <Button type="button" onClick={onClose} className={" absolute righ-2.5 top-3.5"}>X</Button>
             <div className=" flex flex-col absolute lg:w-100 lg:top-4.5 lg:right-1    mt-1 bg-white rounded-lg border shadow-lg overflow-hidden z-50">
-                {posts.map((post) => (
-                    <div className="flex h-15 justify-center items-center text-center w-50 lg:w-full p-2 border-b" onClick={() => handleClick(post.id)}>{post.title}</div>
-                ))}
+                {posts.length > 0 ? (posts.map((post) => (
+                    <div className="flex h-15 justify-center items-center text-center w-50 lg:w-full p-2 border-b" onClick={() => handleClick(post.id)}>{post.title}</div>)
+                )) : 
+                    <p className="flex justify-center p-2" >Không tìm thấy bài viết</p>}
             </div>
 
         </div>
